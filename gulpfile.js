be92fc0e24cd03d5a064bc82240d0024c7b29298
Stuart Lees <stuartlees@gmail.com>
2015-10-23 18:47:28 -0400

    Updating nw builder and fixing build
    - Updating to current nw builder Git repo location
    - Removing "save-version" gulp task as its causing an issue to the build process and the file is adding no value, version is stated in `package.json`
    - nw builder now supports building 32 and 64 bit versions of all three OS
    - applying version `0.12.2` to be used as a workaround to a current bug - https://goo.gl/MX7Lu2

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,13 @@
 /*jshint node: true */
 
 var env = process.env.NODE_ENV || 'dev';
-var NwBuilder = require('node-webkit-builder');
+var NwBuilder = require('nw-builder');
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 var bump = require('gulp-bump');
 var rename = require('gulp-rename');
-var clean = require('gulp-clean');
 var gutil = require('gulp-util');
 var fs = require('fs');
+var del = require("del");
 
 console.log('Environment is', env);
 
@@ -21,21 +20,10 @@ gulp.task('config', function() {
 });
 
 gulp.task('clean', function() {
-  return gulp.src('build/**/*', {read: false})
-    .pipe(clean({force: true}));
+  del(['./build/**']);
 });
 
-gulp.task('save-version', function () {
-  var json = require('./package.json');
-  if(!json.version) {
-    throw 'No version specified in package.json';
-  }
-  else {
-    fs.writeFileSync('build/VERSION', json.version);
-  }
-});
-
-gulp.task('build', ['config', 'clean', 'save-version'], function (callback) {
+gulp.task('build', ['config', 'clean'], function (callback) {
 
     var nw = new NwBuilder({
       files: ['web/**', 'server.js', 'package.json',
@@ -44,7 +32,8 @@ gulp.task('build', ['config', 'clean', 'save-version'], function (callback) {
         'node_modules/sockjs/**',
         'node_modules/open/**'
         ],
-      platforms: ['linux32', 'osx', 'win']
+      platforms: ['linux', 'osx', 'win'],
+      version: '0.12.2' // override version so it stops trying to download the latest - https://goo.gl/MX7Lu2
     });
 
     // Log stuff you want
